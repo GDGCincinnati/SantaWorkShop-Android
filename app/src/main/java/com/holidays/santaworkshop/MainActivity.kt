f04e6bbd2729154d6ca530a02199cf854a5436c3
Sampada Kadam <smpd.kdm@gmail.com>
2022-12-21 11:17:16 -0500

    add firebase function and display images

--- app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
+++ app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
@@ -8,19 +8,33 @@ import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
+import coil.compose.rememberAsyncImagePainter
+import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.holidays.santaworkshop.ui.theme.SantaWorkShopTheme
+import kotlinx.coroutines.launch
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -110,9 +124,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun Home(takePictureOnClick: () -> Unit) {
+    val refreshScope = rememberCoroutineScope()
+    var items by remember {
+        mutableStateOf(listOf<String>())
+    }
+    var refreshing by remember { mutableStateOf(false) }
+
+    fun refresh() = refreshScope.launch {
+        refreshing = true
+        val task = Firebase.functions
+            .getHttpsCallable("getAllImages")
+            .call()
+            .continueWith { task ->
+                // This continuation runs on either success or failure, but if the task
+                // has failed then result will throw an Exception which will be
+                // propagated down.
+                val result = task.result?.data as? List<String> ?: emptyList()
+                result
+            }
+        task.addOnCompleteListener {
+            items = if (it.isSuccessful) {
+                it.result
+            } else {
+                emptyList()
+            }
+            refreshing = false
+        }
+
+    }
+
+
+    val pullRefreshState = rememberPullRefreshState(refreshing, ::refresh)
     Scaffold(
         topBar = {
             TopAppBar(title = {
@@ -142,6 +187,20 @@ fun Home(takePictureOnClick: () -> Unit) {
                 .fillMaxSize()
                 .padding(it),
                 color = MaterialTheme.colorScheme.surface) {
+                Box(Modifier.pullRefresh(pullRefreshState)) {
+                    LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp),
+                        modifier = Modifier.fillMaxSize()) {
+                        items(items = items) { item ->
+                            Image(painter = rememberAsyncImagePainter(model = item),
+                                contentDescription = "",
+                                modifier = Modifier.size(128.dp))
+                        }
+                    }
+
+                    PullRefreshIndicator(refreshing,
+                        pullRefreshState,
+                        Modifier.align(Alignment.TopCenter))
+                }
 
             }
         })
