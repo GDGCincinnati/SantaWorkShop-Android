2464f5157299be0881a6fdcd357f8fcd650ca75d
Sampada Kadam <smpd.kdm@gmail.com>
2022-12-20 20:21:57 -0500

    add onClick functionality

--- app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
+++ app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.holidays.santaworkshop
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +17,13 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.FileProvider
 import com.holidays.santaworkshop.ui.theme.SantaWorkShopTheme
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,17 +31,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             SantaWorkShopTheme {
                 Home(
-
+                    takePictureOnClick = {
+                        dispatchTakePictureIntent()
+                    }
                 )
 
             }
+        }
+    }
+
+
+    val IMAGE_CAPTURE_REQUEST_CODE = 1000
+    private val TAG = "SantaWorkshop"
+    lateinit var currentPhotoPath: String
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager).also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Log.i(TAG, "error during creation of file")
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.holidays.santaworkshop.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home() {
+fun Home(takePictureOnClick: () -> Unit) {
     Scaffold(
         topBar = {
             TopAppBar(title = {
@@ -46,7 +105,7 @@ fun Home() {
         },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {},
+                onClick = takePictureOnClick,
                 shape = CircleShape,
                 contentColor = MaterialTheme.colorScheme.onPrimary,
                 content = {
