b74fb7d5934d28a666f24ca5750ee1d90780a998
Sampada Kadam <smpd.kdm@gmail.com>
2022-12-20 19:50:42 -0500

    add basic UI

--- app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
+++ app/src/main/java/com/holidays/santaworkshop/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,25 +19,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SantaWorkShopTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                Home(
+
+                )
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun Home() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Santa Workshop")
+            },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    scrolledContainerColor = MaterialTheme.colorScheme.surface,
+                    navigationIconContentColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                ))
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                shape = CircleShape,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                content = {
+                    Icon(Icons.Default.Add, "Camera")
+                },
+                containerColor = MaterialTheme.colorScheme.primary)
+        },
+        content = {
+            // A surface container using the 'background' color from the theme
+            Surface(modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+                color = MaterialTheme.colorScheme.surface) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SantaWorkShopTheme {
-        Greeting("Android")
-    }
+            }
+        })
 }
